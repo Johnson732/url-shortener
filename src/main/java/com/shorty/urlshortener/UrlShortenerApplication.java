5f5ead6127143755f1f12b4b21b12a95bb4550ae
Johnson <johnson732j@gmail.com>
2026-05-17 16:34:22 +0530

    exclude jpa

--- src/main/java/com/shorty/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/shorty/urlshortener/UrlShortenerApplication.java
@@ -2,8 +2,17 @@ package com.shorty.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceInitializationAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        DataSourceInitializationAutoConfiguration.class
+})
 public class UrlShortenerApplication {
 
     public static void main(String[] args) {
